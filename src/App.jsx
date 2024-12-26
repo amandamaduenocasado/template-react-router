e19364291cb0d04501b0170e8cf98662fe4b0c41
amandamaduenocasado <amandacaso97@gmail.com>
2024-12-26 18:27:33 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Button from './components/button/Button';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router/Router';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
 	return (
-		<div>
-			<GlobalStyles />
-			<h1>Curso de React</h1>
-			<Button color='steelblue'>Click Me!</Button>
-			<Button color='#f0f'>Click Me!</Button>
-		</div>
+		<BrowserRouter>
+			<GlobalStyles/>
+			<h1></h1>
+			<Router/>
+		</BrowserRouter>
 	);
 };
 
 export default App; 
+
+
